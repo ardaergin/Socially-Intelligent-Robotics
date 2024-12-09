6a1f9e51c3c64befbff87f4f0e252b21d71186f3
ardaergin <ardergin@gmail.com>
2024-12-09 15:15:27 +0100

    Create touch_stop.py

---
+++ touch_stop.py
@@ -0,0 +1,28 @@
+
+from sic_framework.devices import Nao
+
+
+def test_func(a):
+    print("Pressed", a.value)
+
+def touch_stop(sensor):
+    # global interrupted
+    sensor = sensor.value
+    print("Pressed", sensor.value)
+    
+    # # Detect if ANY touch sensor is activated
+    # touch_detection = any(sensor_info[1] for sensor_info in sensor)
+
+    # if touch_detection:
+    #     if verbose_output: print("Touch detected! Stopping current speech and setting interruption flag.")
+    #     nao.tts.request(
+    #         NaoqiTextToSpeechRequest("Oh, I understand. Let me switch to a different time period in Amsterdam."))
+    #     interrupted = True
+
+
+nao = Nao(ip="10.0.0.240")
+
+nao.buttons.register_callback(test_func)
+
+while True:
+    pass  # Keep script alive
